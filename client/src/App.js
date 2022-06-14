beee374ee24386d9b6fd1d4926f8ec6866cf8405
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2022-06-13 20:30:07 -0400

    initialized git and first commit before deployment

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { Helmet } from "react-helmet";
+import PortfolioContainer from "./PortfolioContainer/PortfolioContainer";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>aliciaDevs</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="Developer Porfolio" />
+      </Helmet>
+      <ToastContainer />
+      <PortfolioContainer />
+    </div>
+  );
+}
+
+export default App;
