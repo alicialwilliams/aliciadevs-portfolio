f3e197d8e577064c8d08a6b2a72fcb1dc2cc9437
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2022-06-14 23:51:01 -0400

    more changes to server for heroku deployment

--- server.js
+++ server.js
@@ -7,16 +7,29 @@ const contactRoute = require('./route/contactRoute')
 const app = express();
 
 //middleware
+const whitelist = ['http://localhost:3000', 'http://localhost:3001', 'https://aliciadevs.herokuapp.com/']
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+    origin: function (origin, callback) {
+        console.log("** Origin of request " + origin)
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            console.log("Origin acceptable")
+            callback(null, true)
+        } else {
+            console.log("Origin rejected")
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions));
 
 app.use("/", contactRoute);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
-    );
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client/build", "index.html"))
+    });
 }
 
 const port = process.env.PORT || 3001;
